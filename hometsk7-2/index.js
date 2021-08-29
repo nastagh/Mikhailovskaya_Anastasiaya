2d9a9a4d0d1d8e678c15ff1641d935b259d9feb4
Nastia <nastagh96@mail.ru>
2021-08-29 17:15:38 +0300

    starts

--- hometsk7-2/index.js
+++ hometsk7-2/index.js
@@ -1,26 +1,43 @@
 "use strict"
 
+
+// const imgElem=document.getElementById('picture');
+// console.log(imgElem);
+
+
 document.body.addEventListener('mousedown',mousedown,false);
 document.body.addEventListener('mousemove',mousemove,false);
 document.body.addEventListener('mouseup',mouseup,false);
 
 
-function mousedown (EO) {
-    EO=EO||window.event;
-    var pictureObj=document.getElementById('picture');
-    pictureObj.style.position='absolute';
+let currentPicture;
+let clickX=0;
+let clickY=0;
 
+function mousedown (event) {
+    event=event||window.event;
+    currentPicture=event.target;
+    if (currentPicture.tagName=='IMG') {
+        currentPicture.style.position='absolute';
+        clickX=event.pageX-currentPicture.offsetLeft;
+        clickY=event.pageY-currentPicture.offsetTop;
+    console.log(clickY,clickX);
+    }
+    // console.log(currentPicture);
 }
 
-function mousemove (EO) {
-    EO=EO||window.event;
-    var pictureObj=document.getElementById('picture');
-    pictureObj.style.position='absolute';
-    pictureObj.style.left=EO.pageX+"px";
-    pictureObj.style.top=EO.pageY+"px";
+function mousemove (event) {
+    event=event||window.event;
+    if (currentPicture) {
+        currentPicture.style.left=event.pageX-clickX+"px";
+        currentPicture.style.top=event.pageY-clickY+"px";
+        console.log(currentPicture);
+    }
 }
 
-function mouseup (EO) {
-    EO=EO||window.event;
-
+function mouseup (event) {
+    event=event||window.event;
+    if (currentPicture) {
+        currentPicture=undefined;
+    }
 }
