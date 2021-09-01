89d3df3d4e0dc21c804a39c104ed3fbfc71e7798
Nastia <nastagh96@mail.ru>
2021-09-01 21:47:27 +0300

    finish

--- hometask8-1/index.js
+++ hometask8-1/index.js
@@ -1,11 +1,34 @@
 'use strict'
 
-const hourCount=12;
+const hourCount=12; //количество кружочков
 const clock=document.querySelector('.container');
 const radius=clock.offsetWidth/2;
-const smallRadius=radius/7.5;
-const distance=radius*4/5;
-const sectorAngle=Math.PI*2/hourCount;
+const smallRadius=radius/7.5; //радиус маленьких кружочков
+const distance=radius*4/5; //расстояние между центрами
+const sectorAngle=Math.PI*2/hourCount; //угол сектора
+
+const hourArrowWidth=10;
+const hourArrowLength=radius*0.6;
+const minutArrowWidth=7;
+const minutArrowLength=radius*0.7;
+const secondArrowWidth=5;
+const secondArrowLength=radius*0.8;
+
+function updateTime() {
+    const date=new Date();
+    const hours=date.getHours();
+    const minuts=date.getMinutes();
+    const second=date.getSeconds();
+    const sec=360/60*second;
+    const min=360/60*minuts;
+    const hou=360/hourCount*(hours+minuts/60);
+    hourArrow.style.transform=`rotate(${hou+180}deg)`;
+    minutArrow.style.transform=`rotate(${min+180}deg)`;
+    secondArrow.style.transform=`rotate(${sec+180}deg)`;
+    timeNow.innerHTML=getCurrentTimeString();
+    setTimeout(updateTime,1000-date.getMilliseconds());
+}
+
 
 // рисуем маленькие кружочки
 for (let i=1; i<=hourCount; i++) {
@@ -20,13 +43,9 @@ for (let i=1; i<=hourCount; i++) {
 }
 
 // выставляем время
-
-setInterval(getCurrentTimeString,1000);
-
 const timeNow=document.createElement('span');
 function getCurrentTimeString() {
     let date=new Date();
-    // setTimeout(getCurrentTimeString, 1020-date.getMilliseconds());
     return date.toTimeString().split(" ")[0];
 }
 timeNow.innerHTML=getCurrentTimeString();
@@ -35,66 +54,34 @@ timeNow.style.left=(radius+distance*Math.sin(hourCount*sectorAngle)-smallRadius)
 timeNow.style.top=radius/2+'px';
 clock.appendChild(timeNow);
 
-// setInterval(getCurrentTimeString,1020);
 
 // создаем часовую стрелку
-const hourArrow=document.createElement('div');
-const arrowWidth=10;
-const hourArrowHeight=radius*0.6;
-
-hourArrow.classList.add('arrow');
-hourArrow.style.height=hourArrowHeight+'px';
-hourArrow.style.width=arrowWidth+'px';
-hourArrow.style.left=radius-arrowWidth/2+'px';
-hourArrow.style.top=radius/2+'px';
-hourArrow.style.transformOrigin=`center ${hourArrowHeight-arrowWidth*1.5}px`;
-
+const hourArrow=createArrow(radius, hourArrowWidth, hourArrowLength);
 clock.appendChild(hourArrow);
 
 // создаем минутную стрелку
-const minutArrow=document.createElement('div');
-const minutWidth=7;
-const minutArrowHeight=radius*0.7;
-minutArrow.classList.add('arrow');
-minutArrow.style.height=minutArrowHeight+'px';
-minutArrow.style.width=minutWidth+'px';
-minutArrow.style.left=radius-minutWidth/2+'px';
-minutArrow.style.top=radius/3.3+'px';
-minutArrow.style.transformOrigin=`center ${minutArrowHeight-minutWidth*1.5}px`;
-// minutArrow.style.transform='rotate(60deg)';
+const minutArrow=createArrow(radius, minutArrowWidth, minutArrowLength);
 clock.appendChild(minutArrow);
 
+
 //создаем секундную стрелку
-const secondArrow=document.createElement('div');
-const secondWidth=5;
-const secondArrowHeight=radius*0.8;
-secondArrow.classList.add('arrow');
-secondArrow.style.height=secondArrowHeight+'px';
-secondArrow.style.width=secondWidth+'px';
-secondArrow.style.left=radius-secondWidth/2+'px';
-secondArrow.style.top=radius/3.8+'px';
-secondArrow.style.transformOrigin=`center ${secondArrowHeight-secondWidth*1.5}px`;
-
+const secondArrow=createArrow(radius, secondArrowWidth, secondArrowLength);
 clock.appendChild(secondArrow);
+updateTime();
 
 
-
-function updateTime() {
-    const date=new Date();
-    const hours=date.getHours();
-    const minuts=date.getMinutes();
-    const second=date.getSeconds();
-    const sec=360/60*second;
-    const min=360/60*minuts;
-    const hou=360/hourCount*(hours+minuts/60);
-    hourArrow.style.transform=`rotate(${hou}deg)`;
-    minutArrow.style.transform=`rotate(${min}deg)`;
-    secondArrow.style.transform=`rotate(${sec}deg)`;
+function createArrow(clockRadius,width,length) {
+    const arrow=document.createElement('div');
+    arrow.classList.add('arrow');
+    arrow.style.width=width+'px';
+    arrow.style.height=length+'px';
+    arrow.style.left=clockRadius-width/2+'px';
+    arrow.style.top=clockRadius-width/2+'px';
+    arrow.style.transformOrigin=`center ${width/2}px`;
+    return arrow
 }
 
-setInterval(updateTime,1000);
 
-// сделать через центр!!!!
 
 
 
